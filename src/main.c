c5a98a6c8552621513e97d5b6470b0e2307960dd
Robert Ancell <robert.ancell@canonical.com>
2021-09-20 14:41:18 +1200

    Code style consistency

--- src/main.c
+++ src/main.c
@@ -3,55 +3,48 @@
 #include "config.h"
 #include "ua-ubuntu-advantage-generated.h"
 
-static gboolean ua_ubuntu_advantage_attach(UaUbuntuAdvantage *skeleton,
-                                           GDBusMethodInvocation *invocation,
-                                           const gchar *token,
-                                           gpointer user_data) {
+static gboolean attach_cb(UaUbuntuAdvantage *skeleton,
+                          GDBusMethodInvocation *invocation, const gchar *token,
+                          gpointer user_data) {
   ua_ubuntu_advantage_complete_attach(skeleton, invocation);
 
   return TRUE;
 }
 
-static gboolean ua_ubuntu_advantage_detach(UaUbuntuAdvantage *skeleton,
-                                           GDBusMethodInvocation *invocation,
-                                           gpointer user_data) {
+static gboolean detach_cb(UaUbuntuAdvantage *skeleton,
+                          GDBusMethodInvocation *invocation,
+                          gpointer user_data) {
   ua_ubuntu_advantage_complete_detach(skeleton, invocation);
 
   return TRUE;
 }
 
-static gboolean ua_ubuntu_advantage_enable(UaUbuntuAdvantage *skeleton,
-                                           GDBusMethodInvocation *invocation,
-                                           const gchar *service_name,
-                                           gpointer user_data) {
+static gboolean enable_cb(UaUbuntuAdvantage *skeleton,
+                          GDBusMethodInvocation *invocation,
+                          const gchar *service_name, gpointer user_data) {
   ua_ubuntu_advantage_complete_enable(skeleton, invocation);
 
   return TRUE;
 }
 
-static gboolean ua_ubuntu_advantage_disable(UaUbuntuAdvantage *skeleton,
-                                            GDBusMethodInvocation *invocation,
-                                            const gchar *service_name,
-                                            gpointer user_data) {
+static gboolean disable_cb(UaUbuntuAdvantage *skeleton,
+                           GDBusMethodInvocation *invocation,
+                           const gchar *service_name, gpointer user_data) {
   ua_ubuntu_advantage_complete_disable(skeleton, invocation);
 
   return TRUE;
 }
 
-static void on_bus_acquired(GDBusConnection *connection, const gchar *name,
+static void bus_acquired_cb(GDBusConnection *connection, const gchar *name,
                             gpointer user_data) {
   g_autoptr(GError) error = NULL;
   g_autoptr(UaUbuntuAdvantage) ua = ua_ubuntu_advantage_skeleton_new();
   ua_ubuntu_advantage_set_daemon_version(UA_UBUNTU_ADVANTAGE(ua),
                                          PROJECT_VERSION);
-  g_signal_connect(ua, "handle-attach", G_CALLBACK(ua_ubuntu_advantage_attach),
-                   NULL);
-  g_signal_connect(ua, "handle-detach", G_CALLBACK(ua_ubuntu_advantage_detach),
-                   NULL);
-  g_signal_connect(ua, "handle-enable", G_CALLBACK(ua_ubuntu_advantage_enable),
-                   NULL);
-  g_signal_connect(ua, "handle-disable",
-                   G_CALLBACK(ua_ubuntu_advantage_disable), NULL);
+  g_signal_connect(ua, "handle-attach", G_CALLBACK(attach_cb), NULL);
+  g_signal_connect(ua, "handle-detach", G_CALLBACK(detach_cb), NULL);
+  g_signal_connect(ua, "handle-enable", G_CALLBACK(enable_cb), NULL);
+  g_signal_connect(ua, "handle-disable", G_CALLBACK(disable_cb), NULL);
   if (!g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON(ua),
                                         connection, "/", &error)) {
     g_printerr("Failed to export D-Bus object: %s", error->message);
@@ -59,7 +52,7 @@ static void on_bus_acquired(GDBusConnection *connection, const gchar *name,
   }
 }
 
-static void on_name_lost(GDBusConnection *connection, const gchar *name,
+static void name_lost_cb(GDBusConnection *connection, const gchar *name,
                          gpointer user_data) {
   GMainLoop *loop = user_data;
 
@@ -70,8 +63,8 @@ int main(int argc, char **argv) {
   g_autoptr(GMainLoop) loop = g_main_loop_new(NULL, FALSE);
 
   g_bus_own_name(G_BUS_TYPE_SYSTEM, "com.example.UbuntuAdvantage",
-                 G_BUS_NAME_OWNER_FLAGS_NONE, on_bus_acquired, NULL,
-                 on_name_lost, loop, NULL);
+                 G_BUS_NAME_OWNER_FLAGS_NONE, bus_acquired_cb, NULL,
+                 name_lost_cb, loop, NULL);
 
   g_main_loop_run(loop);
 
